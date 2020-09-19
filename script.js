e575faac9250369503ec171270a68b334b42e1af
ClickDaHouseCat <63846200+ClickDaHouseCat@users.noreply.github.com>
2020-09-19 13:57:22 +0700

    Все работает
    Все работает

--- script.js
+++ script.js
@@ -37,24 +37,65 @@ const quizData = [
     }
 ]
 
-const questionEl = document.getElementById('question')
+const quiz = document.getElementById("quiz")
+const answerEls = document.querySelectorAll(".answer")
+const questionEl = document.getElementById("question")
 
-a_text = document.getElementById('a_text')
-b_text = document.getElementById('b_text')
-c_text = document.getElementById('c_text')
-d_text = document.getElementById('d_text')
+const a_text = document.getElementById('a_text')
+const b_text = document.getElementById('b_text')
+const c_text = document.getElementById('c_text')
+const d_text = document.getElementById('d_text')
+const submitBtn = document.getElementById("submit")
 
 let currentQuiz = 0
+let score = 0
 
 loadQuiz ();
 
 function loadQuiz() {
-    const currentQuizData = quizData[currentQuestion]
+    deselectAnswers()
+
+    const currentQuizData = quizData[currentQuiz]
+
     questionEl.innerText = currentQuizData.question
     a_text.innerText = currentQuizData.a
     b_text.innerText = currentQuizData.b
     c_text.innerText = currentQuizData.c
     d_text.innerText = currentQuizData.d
-
-    currentQuestion++
 }
+
+function getSelected (){
+    let answer = undefined
+
+    answerEls.forEach((answerEl) => {
+        if (answerEl.checked) {
+            answer = answerEl.id
+        }
+    })
+    return answer
+}
+
+function deselectAnswers(){
+    answerEls.forEach((answerEl) => {
+        answerEl.checked = false
+    })
+}
+
+submitBtn.addEventListener("click", () => {
+    //Проверка видимости вопроса
+    const answer = getSelected()
+
+    if(answer) {
+        if (answer === quizData[currentQuiz].correct){
+            score++
+        }
+
+        currentQuiz++
+        if (currentQuiz < quizData.length) {
+            loadQuiz()
+        }else {
+            quiz.innerHTML = `<h2>Количество верных ответов ${score}/${quizData.length}</h2> <button onclick="location.reload()">Перезапуск</button>`
+
+        }
+    }
+})
